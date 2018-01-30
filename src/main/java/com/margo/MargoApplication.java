08431a9b96b937dfbf0163fc62f1f1e0a7ec2dd0
pglg95 <pglg95@op.pl>
2018-01-30 18:19:35 +0100

    Initial commit

---
+++ src/main/java/com/margo/MargoApplication.java
@@ -0,0 +1,12 @@
+package com.margo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MargoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MargoApplication.class, args);
+	}
+}
