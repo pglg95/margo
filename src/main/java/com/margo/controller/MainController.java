885aed79917d7643bdd6f936cb13c8ce1da093f8
pglg95 <pglg95@op.pl>
2018-01-30 23:55:56 +0100

    fix hot

--- src/main/java/com/margo/controller/MainController.java
+++ src/main/java/com/margo/controller/MainController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 /**
  * Created by pglg on 30.01.2018.
  */
@@ -17,12 +19,12 @@ public class MainController {
     @Autowired
     private MeasurementDataService measurementDataService;
 
-    @GetMapping("/measurements/cities/{city}/paramTypes/{paramType}")
+    @GetMapping("/measurements/cities/{city}/paramTypes/{paramType:.+}")
     public String getAvgMeasurementFromStationsInCity(@PathVariable String city,
                                                       @PathVariable String paramType){
 
         Double measurement = measurementDataService.getMeasurement(city, paramType);
 
-        return String.format("%.2f",measurement);
+        return String.format(Locale.US,"%.2f",measurement);
     }
 }
